44a3eab4d320964b48c8b90ec08ddce44d451d71
Saul Alanis <71178706+salanisor@users.noreply.github.com>
2026-03-22 20:55:46 -0500

    feat: phase 3 complete - alb controller, ingress, networkpolicies

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -118,6 +118,10 @@ resource "aws_eks_addon" "vpc_cni" {
   cluster_name  = aws_eks_cluster.main.name
   addon_name    = "vpc-cni"
   resolve_conflicts_on_create = "OVERWRITE"
+
+  configuration_values = jsonencode({
+    enableNetworkPolicy = "true"
+  })
 }
 
 resource "aws_eks_addon" "coredns" {
